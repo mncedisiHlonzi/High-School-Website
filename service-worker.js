d7bc2125175d48b50f4cef0684b1aa8d5b899a42
mncedisi <mncedisinhlonzi37@gmail.com>
2025-08-03 11:49:42 +0200

    Commited changes

---
+++ service-worker.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'shengeza-cache-v1';
+const OFFLINE_URL = '/offline.html';
+
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/manifest.json',
+  '/css/style.css',
+  '/js/script.js',
+  '/js/swiper-bundle.min.js', 
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+  '/maskable-icon.png'
+];
+
+// Install Service Worker and Cache Assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate and clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch Event: Serve cached assets or go to network
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(OFFLINE_URL);
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
